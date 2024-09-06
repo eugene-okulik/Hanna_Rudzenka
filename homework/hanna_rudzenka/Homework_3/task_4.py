26d2e8e39266c2892d822e019dab7263742ec95c
Hanna <annpetlitskaya@gmail.com>
2024-09-06 21:06:45 +0200

    fixed lint failure in task_4

--- homework/hanna_rudzenka/Homework_3/task_4.py
+++ homework/hanna_rudzenka/Homework_3/task_4.py
@@ -1,6 +1,6 @@
 side_1 = 5
 side_2 = 5
-hypotenuse = (side_1**2 + side_2**2)**0.5
-square = side_1*side_2/2
+hypotenuse = (side_1 ** 2 + side_2 ** 2) ** 0.5
+square = side_1 * side_2 / 2
 print('hypotenuse:', hypotenuse)
 print('square:', square)
