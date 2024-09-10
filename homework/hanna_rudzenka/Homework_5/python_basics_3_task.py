70f6bdaf5a98208a7f8ca62eba842f171668f743
Hanna <annpetlitskaya@gmail.com>
2024-09-10 13:54:21 +0200

    fixed task 1

--- homework/hanna_rudzenka/Homework_5/python_basics_3_task.py
+++ homework/hanna_rudzenka/Homework_5/python_basics_3_task.py
@@ -5,9 +5,9 @@ res_1 = 'результат операции: 42'
 res_2 = 'результат операции: 514'
 res_3 = 'результат работы программы: 9'
 
-res_1 = int(res_1[res_1.index('42'):])
-res_2 = int(res_2[res_2.index('514'):])
-res_3 = int(res_3[res_3.index('9'):])
+res_1 = int(res_1[res_1.index(':') + 2:])
+res_2 = int(res_2[res_2.index(':') + 2:])
+res_3 = int(res_3[res_3.index(':') + 2:])
 
 print(res_1 + 10)
 print(res_2 + 10)
