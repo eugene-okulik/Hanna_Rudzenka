c114714c98f9f2c8d1a8adae9b08c6ee8a94a9ce
Hanna <annpetlitskaya@gmail.com>
2024-10-04 22:19:17 +0200

    api task is done

---
+++ homework/hanna_rudzenka/Homework_18/API_task.py
@@ -0,0 +1,58 @@
+import requests
+
+base_url = 'http://167.172.172.115:52353/object'
+
+
+def create_new_object_and_return_id():
+    body = {
+        "name": "Ivan",
+        "data": {
+            "age": 30,
+            "job": "doctor"
+        }
+    }
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(base_url, json=body, headers=headers).json()
+    return response['id']
+
+
+def clear_object(object_id):
+    requests.delete(f'{base_url}/{object_id}')
+
+
+def update_new_object_with_put_method():
+    object_id = create_new_object_and_return_id()
+    body = {
+        "name": "Ivan",
+        "data": {
+            "age": 30,
+            "job": "doctor",
+            "location": "Minsk"
+        }
+    }
+    headers = {"Content-Type": "application/json"}
+    response = requests.put(f'{base_url}/{object_id}', json=body, headers=headers).json()
+    assert response['data']['location'] == 'Minsk', "object location is incorrect"
+    clear_object(object_id)
+
+
+def update_new_object_with_patch_method():
+    object_id = create_new_object_and_return_id()
+    body = {
+        "data": {
+            "age": 31,
+            "job": "doctor"
+        }
+    }
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.patch(f'{base_url}/{object_id}', json=body, headers=headers).json()
+    assert response['data']['age'] == 31, 'object age is incorrect'
+    clear_object(object_id)
+
+
+def delete_object():
+    object_id = create_new_object_and_return_id()
+    requests.delete(f'{base_url}/{object_id}')
+    response = requests.get(f'{base_url}/{object_id}')
+    assert response.status_code == 404
