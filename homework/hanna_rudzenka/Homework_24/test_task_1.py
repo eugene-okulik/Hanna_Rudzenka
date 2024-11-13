1a4f69725d616be8c5747339dd784403cbf061cc
Hanna <annpetlitskaya@gmail.com>
2024-11-13 09:37:58 +0100

    test_2 is updated with explicit wait

--- homework/hanna_rudzenka/Homework_24/test_task_1.py
+++ homework/hanna_rudzenka/Homework_24/test_task_1.py
@@ -5,24 +5,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.wait import WebDriverWait as Wait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
 
 def test_1():
     driver = Chrome()
-    timeout = 3
-    driver.implicitly_wait(timeout)
+    wait = Wait(driver, timeout=3)
     driver.get('https://www.demoblaze.com/index.html')
-    samsung_galaxy_s6 = driver.find_element(By.LINK_TEXT, 'Samsung galaxy s6')
+    samsung_galaxy_s6 = wait.until(ec.presence_of_element_located((By.LINK_TEXT, 'Samsung galaxy s6')))
     ActionChains(driver).key_down(Keys.CONTROL).click(samsung_galaxy_s6).key_up(Keys.CONTROL).perform()
     tabs = driver.window_handles
     driver.switch_to.window(tabs[1])
-    driver.find_element(By.LINK_TEXT, "Add to cart").click()
+    wait.until(ec.presence_of_element_located((By.LINK_TEXT, "Add to cart"))).click()
     alert = Alert(driver)
-    Wait(driver, timeout=timeout).until(EC.alert_is_present())
+    wait.until(ec.alert_is_present())
     alert.accept()
     driver.close()
     driver.switch_to.window(tabs[0])
     driver.find_element(By.LINK_TEXT, "Cart").click()
-    item_text = driver.find_element(By.CSS_SELECTOR, "#tbodyid > tr > td:nth-child(2)").text
+    item_text = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "#tbodyid > tr > td:nth-child(2)"))).text
     assert item_text == "Samsung galaxy s6"
